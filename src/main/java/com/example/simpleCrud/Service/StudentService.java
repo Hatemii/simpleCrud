84da0cff2f4eecd21f4c15e71840a7ed544e82f2
Hatemii <hatem.xhezairi96@gmail.com>
2020-10-14 13:43:21 +0200

    StudentService class

--- src/main/java/com/example/simpleCrud/Service/StudentService.java
+++ src/main/java/com/example/simpleCrud/Service/StudentService.java
@@ -1,4 +1,14 @@
 package com.example.simpleCrud.Service;
 
+import com.example.simpleCrud.Dao.StudentDao;
+import com.example.simpleCrud.Entity.Student;
+
+import java.util.Collection;
+
 public class StudentService {
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents(){
+        return this.studentDao.getAllStudents();
+    }
 }
