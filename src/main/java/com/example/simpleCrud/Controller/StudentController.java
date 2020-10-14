302d12a67944c528215e54378121241154b732b2
Hatemii <hatem.xhezairi96@gmail.com>
2020-10-14 13:45:23 +0200

    StudentController class

--- src/main/java/com/example/simpleCrud/Controller/StudentController.java
+++ src/main/java/com/example/simpleCrud/Controller/StudentController.java
@@ -1,4 +1,15 @@
 package com.example.simpleCrud.Controller;
 
+import com.example.simpleCrud.Entity.Student;
+import com.example.simpleCrud.Service.StudentService;
+
+import java.util.Collection;
+
 public class StudentController {
+    private StudentService studentService;
+
+    public Collection<Student> getAllStudents(){
+        return this.studentService.getAllStudents();
+    }
+
 }
