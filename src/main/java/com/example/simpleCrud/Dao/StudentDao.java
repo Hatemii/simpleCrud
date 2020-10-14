b295874666177f29c2c37fd8f5488dd3a55867c1
Hatemii <hatem.xhezairi96@gmail.com>
2020-10-14 13:40:45 +0200

    StudentDao class

--- src/main/java/com/example/simpleCrud/Dao/StudentDao.java
+++ src/main/java/com/example/simpleCrud/Dao/StudentDao.java
@@ -1,4 +1,22 @@
 package com.example.simpleCrud.Dao;
 
+import com.example.simpleCrud.Entity.Student;
+import java.util.*;
+
 public class StudentDao {
+    private static Map<Integer, Student> students;
+
+    static{
+        students = new HashMap<Integer,Student>(){
+            {
+                put(1,new Student(1,"Lebron","Computer Science"));
+                put(2,new Student(2,"Steph","Computer Network"));
+                put(3,new Student(3,"Damian","Computer Technology"));
+            }
+        };
+    }
+
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    }
 }
